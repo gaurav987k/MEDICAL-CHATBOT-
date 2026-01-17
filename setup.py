b36b5f9860d9b48fe3af1cc867f122f9d032d5e5
Gaurav Kanojiya <gauravkanojiya575@gmail.com>
2026-01-17 23:33:58 +0530

    u[date

--- setup.py
+++ setup.py
@@ -5,6 +5,18 @@ setup(
     version="0.1.0",
     author="Boktiar Ahmed Bappy",
     author_email="entbappy73@gmail.com",
+    description="Medical Chatbot using LangChain and HuggingFace",
     packages=find_packages(),
-    install_requires=[]
+    install_requires=[
+        "python-dotenv",
+        "langchain",
+        "langchain-community",
+        "langchain-huggingface",
+        "huggingface-hub",
+        "pypdf",
+        "faiss-cpu",
+        "numpy",
+        "tqdm"
+    ],
+    python_requires=">=3.10"
 )
